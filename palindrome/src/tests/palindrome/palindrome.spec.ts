722f6abae0c049c0361a615ad7e1b98cc7f72220
Kevin Do <do.kevin.vo@gmail.com>
2026-04-19 10:37:35 -0700

    test: adds failing test for palindromeChecker

---
+++ palindrome/src/tests/palindrome/palindrome.spec.ts
@@ -0,0 +1,5 @@
+describe('Palindrome checker', () => {
+  it("should be able to tell that 'mom' is a palindrome", () => {
+    expect(palindromeChecker.isPalindrome('mom')).toBeTruthy();
+  });
+});
