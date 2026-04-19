1d164023075989030aee2811fbca83c7a4bffd8c
Kevin Do <do.kevin.vo@gmail.com>
2026-04-19 11:38:33 -0700

    feat: joins reversed string to pass test see if phrase is a palindrome

--- palindrome/src/utils/palindromeChecker.ts
+++ palindrome/src/utils/palindromeChecker.ts
@@ -1,9 +1,10 @@
 class PalindromeChecker {
   isPalindrome = (str: string): boolean => {
-    const originalString = str.toLowerCase();
+    const originalString = str.toLowerCase().split(' ').join('');
+
     const reversedString = str.toLowerCase().split('').reverse().join('');
 
-    const result = originalString === reversedString;
+    const result = originalString === reversedString.split(' ').join('');
 
     return result;
   };
