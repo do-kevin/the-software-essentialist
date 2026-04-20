11792061f39d6fba2554eaa27d417893de3cd8f6
Kevin Do <do.kevin.vo@gmail.com>
2026-04-19 18:42:55 -0700

    feat: throws Error when given a non-number to pass test

--- fizzbuzz/src/tests/fizzbuzz/fizzbuzz.spec.ts
+++ fizzbuzz/src/tests/fizzbuzz/fizzbuzz.spec.ts
@@ -28,6 +28,53 @@ describe('FizzBuzz', () => {
   it('should return "43" when it receives 43', () => {
     const result = fizzBuzz(43);
 
+    expect(typeof result).toBe('string');
     expect(result).toBe('43');
+  });
+
+  it('should return "Fizz" when it receives 42', () => {
+    const result = fizzBuzz(42);
+
+    expect(result).toBe('Fizz');
+  });
+
+  it('should return "FizzBuzz" when it receives 45', () => {
+    const result = fizzBuzz(45);
+
+    expect(result).toBe('FizzBuzz');
+  });
+
+  it('should return "Fizz" when it receives 102', () => {
+    const result = fizzBuzz(102);
+
+    expect(result).toBe('Fizz');
+  });
+
+  it('should return "Fizz" when it receives -12', () => {
+    const result = fizzBuzz(-12);
+
+    expect(result).toBe('Fizz');
+  });
+
+  it('should number in string if not divisble by 3 or 5', () => {
+    const result = fizzBuzz(41.0);
+
+    expect(result).toBe('41');
+  });
+
+  it('should throw error if given a non-number', () => {
+    expect(() => {
+      fizzBuzz('41');
+    }).toThrow('Received is not of type "number"');
+  });
+
+  it('should throw error if given null or undefined', () => {
+    expect(() => {
+      fizzBuzz(null);
+    }).toThrow('Received is not of type "number"');
+
+    expect(() => {
+      fizzBuzz(undefined);
+    }).toThrow('Received is not of type "number"');
   });
 });
--- fizzbuzz/src/utils/fizzbuzzGame.ts
+++ fizzbuzz/src/utils/fizzbuzzGame.ts
@@ -1,4 +1,8 @@
-export const fizzBuzz = (value: number) => {
+export const fizzBuzz = (value: any) => {
+  if (typeof value !== 'number') {
+    throw new Error('Received is not of type "number"');
+  }
+
   if (value % 5 === 0 && value % 3 === 0) {
     return 'FizzBuzz';
   }
