64027fabe28b7e0c02c3b0d47bc3a1ce8f2d6bb7
Kevin Do <do.kevin.vo@gmail.com>
2026-04-19 18:13:45 -0700

    feat: fizzBuzz returns Buzz when set to 5

--- fizzbuzz/src/tests/fizzbuzz/fizzbuzz.spec.ts
+++ fizzbuzz/src/tests/fizzbuzz/fizzbuzz.spec.ts
@@ -1,11 +1,15 @@
 import { fizzBuzz } from '../../utils/fizzbuzzGame';
 
 describe('FizzBuzz', () => {
-  // beforeEach(() => {});
-
   it('should return "Fizz" when it receives 3', () => {
     const result = fizzBuzz(3);
 
     expect(result).toBe('Fizz');
+  });
+
+  it('should return "Buzz" when it receives 5', () => {
+    const result = fizzBuzz(5);
+
+    expect(result).toBe('Buzz');
   });
 });
