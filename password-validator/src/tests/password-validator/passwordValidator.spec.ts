5ad874b06a779f2e4ed7304677f27ffb474444f4
Kevin Do <do.kevin.vo@gmail.com>
2026-04-26 14:03:54 -0700

    feat: can check if password is outside of the valid range

--- password-validator/src/tests/password-validator/passwordValidator.spec.ts
+++ password-validator/src/tests/password-validator/passwordValidator.spec.ts
@@ -8,14 +8,23 @@ describe('Password Validator', () => {
   });
 
   it("should determine whether the password's length is outside of 5 and 15", () => {
-    const result = passwordValidator.validate();
+    const result = passwordValidator.validate('password123');
 
     expect(result.data.password.length).toBeGreaterThanOrEqual(5);
     expect(result.data.password.length).toBeLessThanOrEqual(15);
   });
 
-  it('should determine that the password is too short', () => {
-    const result = passwordValidator.validate();
+  it('should contain error if the password is too short', () => {
+    const result = passwordValidator.validate('pass');
+
+    expect(result.errors).toContainEqual({
+      code: 'INVALID_LENGTH',
+      message: 'The password must be between 5 and 15 characters long.',
+    });
+  });
+
+  it('should contain error if the password is too long', () => {
+    const result = passwordValidator.validate('longpassword1234');
 
     expect(result.errors).toContainEqual({
       code: 'INVALID_LENGTH',
--- password-validator/src/utils/passwordValidator.ts
+++ password-validator/src/utils/passwordValidator.ts
@@ -1,11 +1,27 @@
+type ErrorMessage = {
+  code: string;
+  message: string;
+};
+
+type PasswordErrors = ErrorMessage[];
+
 class PasswordValidator {
-  validate = (input?: string) => {
+  validate = (input: string) => {
+    const errors: PasswordErrors = [];
+
+    if (!(input.length >= 5 && input.length <= 15)) {
+      errors.push({
+        code: 'INVALID_LENGTH',
+        message: 'The password must be between 5 and 15 characters long.',
+      });
+    }
+
     return {
       data: {
         // pretend this is salted
-        password: 'password123',
+        password: input,
       },
-      errors: [],
+      errors,
     };
   };
 }
