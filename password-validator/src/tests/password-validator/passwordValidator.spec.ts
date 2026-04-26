4a9b6ba45f0ff4d892827a828c5d5af4c58ab61c
Kevin Do <do.kevin.vo@gmail.com>
2026-04-26 11:08:04 -0700

    test: adds failing tests for password length

--- password-validator/src/tests/password-validator/passwordValidator.spec.ts
+++ password-validator/src/tests/password-validator/passwordValidator.spec.ts
@@ -1,7 +1,10 @@
 import { passwordValidator } from '../../utils/passwordValidator';
 
+const password = '';
+
 describe('Password Validator', () => {
-  it('', () => {
-    expect('').toBe('');
+  it("should determine whether the password's length is outside of 5 and 15", () => {
+    expect(password.length).toBeGreaterThanOrEqual(5);
+    expect(password.length).toBeLessThanOrEqual(15);
   });
 });
