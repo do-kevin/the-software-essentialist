23a5c4031913c23d7b9023430eab38f17d323628
Kevin Do <do.kevin.vo@gmail.com>
2026-05-13 00:22:43 -0700

    test: adds additional case for determining min value

--- stats-calculator/src/tests/statsCalculator/statsCalculator.spec.ts
+++ stats-calculator/src/tests/statsCalculator/statsCalculator.spec.ts
@@ -1,10 +1,17 @@
 import { StatsCalculator } from '../../utils/statsCalculator';
 
 describe('Stats Calculator', () => {
-  it('should determine the minimum value is -8', () => {
+  it('should determine the minimum value is -8 from [2, 4, 21, -8, 53, 40]', () => {
     const statsCalculator = new StatsCalculator();
 
     const result = statsCalculator.calculate([2, 4, 21, -8, 53, 40]);
     expect(result.minVal).toBe(-8);
+  });
+
+  it('should determine the minimum value is -1 from [1, 2, 3, 4, 5, -1]', () => {
+    const statsCalculator = new StatsCalculator();
+
+    const result = statsCalculator.calculate([1, 2, 3, 4, 5, -1]);
+    expect(result.minVal).toBe(-1);
   });
 });
