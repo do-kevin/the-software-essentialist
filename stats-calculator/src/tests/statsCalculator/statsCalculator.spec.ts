5c5bf800b1cb04fcf6623e7511b1750329904b53
Kevin Do <do.kevin.vo@gmail.com>
2026-05-13 00:18:40 -0700

    test: adds fail case for min value

--- stats-calculator/src/tests/statsCalculator/statsCalculator.spec.ts
+++ stats-calculator/src/tests/statsCalculator/statsCalculator.spec.ts
@@ -1,5 +1,6 @@
 describe('Stats Calculator', () => {
-  it('', () => {
-    expect(true).toBe(true);
+  it('should determine the minimum value is -8', () => {
+    const result = StatsCalculator.calculate([2, 4, 21, -8, 53, 40]);
+    expect(result.minVal).toBe(-8);
   });
 });
