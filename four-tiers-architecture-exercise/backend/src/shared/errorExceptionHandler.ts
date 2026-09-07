4f61508d185c829fedbc9380c40b6bb2bbd8037a
Kevin Do <do.kevin.vo@gmail.com>
2026-09-07 11:19:07 -0700

    update: separate out error exceptions

---
+++ four-tiers-architecture-exercise/backend/src/shared/errorExceptionHandler.ts
@@ -0,0 +1,70 @@
+import { Request, Response, NextFunction } from "express";
+import { ErrorExceptionType } from "./constants";
+import {
+  AssignmentNotFoundException,
+  ClassNotFoundException,
+  InvalidRequestBodyException,
+  StudentAssignmentNotFoundException,
+  StudentNotFoundException,
+} from "./exceptions";
+
+export class ErrorExceptionHandler {
+  public handle(
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response {
+    if (error instanceof InvalidRequestBodyException) {
+      return res.status(400).json({
+        error: ErrorExceptionType.ValidationError,
+        data: undefined,
+        success: false,
+        message: error.message,
+      });
+    }
+
+    if (error instanceof StudentNotFoundException) {
+      return res.status(400).json({
+        error: ErrorExceptionType.StudentNotFound,
+        data: undefined,
+        success: false,
+        message: error.message,
+      });
+    }
+
+    if (error instanceof ClassNotFoundException) {
+      return res.status(400).json({
+        error: ErrorExceptionType.ClassNotFound,
+        data: undefined,
+        success: false,
+        message: error.message,
+      });
+    }
+
+    if (error instanceof AssignmentNotFoundException) {
+      return res.status(400).json({
+        error: ErrorExceptionType.AssignmentNotFound,
+        data: undefined,
+        success: false,
+        message: error.message,
+      });
+    }
+
+    if (error instanceof StudentAssignmentNotFoundException) {
+      return res.status(400).json({
+        error: ErrorExceptionType.StudentAssignmentNotFound,
+        data: undefined,
+        success: false,
+        message: error.message,
+      });
+    }
+
+    return res.status(500).json({
+      error: ErrorExceptionType.ServerError,
+      data: undefined,
+      success: false,
+      message: error.message,
+    });
+  }
+}
---
+++ four-tiers-architecture-exercise/backend/src/shared/exceptions.ts
@@ -0,0 +1,35 @@
+export class InvalidRequestBodyException extends Error {
+  constructor(missingKeys: string[]) {
+    super("Body is missing required key: " + missingKeys.join(", "));
+  }
+}
+
+export class StudentNotFoundException extends Error {
+  constructor() {
+    super("Student is not found.");
+  }
+}
+
+export class ClassNotFoundException extends Error {
+  constructor(id: string) {
+    super(`Class with ${id} is not found.`);
+  }
+}
+
+export class AssignmentNotFoundException extends Error {
+  constructor() {
+    super(`Assignment is not found.`);
+  }
+}
+
+export class StudentAssignmentNotFoundException extends Error {
+  constructor() {
+    super(`Student's assignment is not found.`);
+  }
+}
+
+export class StudentAlreadyEnrolledException extends Error {
+  constructor() {
+    super("Student is already enrolled in the class.");
+  }
+}
